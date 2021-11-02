53ba187246207317df5558cffae2987d45a84f3d
Simon Gasse <sgasse@users.noreply.github.com>
2021-11-02 22:17:40 +0100

    Stream webcam continuously to browser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use actix_web::App;
 use actix_web::HttpServer;
-use libwebcam_onnx::routes::{index, video};
+use libwebcam_onnx::routes::{index, video_stream};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(video))
+    HttpServer::new(|| App::new().service(index).service(video_stream))
         .bind("127.0.0.1:8080")?
         .run()
         .await
--- src/routes.rs
+++ src/routes.rs
@@ -1,39 +1,58 @@
-use actix_web::body::Body;
-use actix_web::{get, Error, HttpRequest, HttpResponse, Responder};
+use actix_web::web::Bytes;
+use actix_web::{get, Error, HttpResponse, Responder};
+use futures_core::task::{Context, Poll};
+use futures_core::Stream;
 use rscam::Frame;
-use std::future::{ready, Ready};
+use std::pin::Pin;
 
 use super::sensors::get_frame_fn;
 
 #[get("/index")]
 async fn index() -> impl Responder {
-    format!("Hello from actix!")
+    let resp = r#"
+    <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8  offset-lg-2">
+                <h3 class="mt-5">Live Streaming</h3>
+                <img src="./video" width="100%">
+            </div>
+        </div>
+    </div>
+    </body>
+    "#;
+    resp
 }
 
-struct VideoFrame {
-    frame: Frame,
+#[get("/video_stream")]
+async fn video_stream() -> HttpResponse {
+    let cam_stream = StreamableCamera {
+        gen_frame: get_frame_fn(),
+    };
+
+    HttpResponse::Ok()
+        .content_type("multipart/x-mixed-replace; boundary=frame")
+        .streaming(cam_stream)
 }
 
-impl Responder for VideoFrame {
-    type Error = Error;
-    type Future = Ready<Result<HttpResponse, Error>>;
+struct StreamableCamera {
+    gen_frame: Box<dyn Fn() -> Frame>,
+}
 
-    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
-        let body: Vec<u8> = [
-            "--frame\r\nContent-Type: image/jpeg\r\n\r\n".as_bytes(),
-            &self.frame[..],
-            "\r\n\r\n".as_bytes(),
-        ]
-        .concat();
+impl Stream for StreamableCamera {
+    type Item = Result<Bytes, Error>;
 
-        ready(Ok(HttpResponse::Ok()
-            .content_type("multipart/x-mixed-replace; boundary=frame")
-            .body(Body::from_slice(&body[..]))))
+    fn poll_next(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let frame = (*self.gen_frame)();
+        let body: Bytes = Bytes::copy_from_slice(
+            &[
+                "--frame\r\nContent-Type: image/jpeg\r\n\r\n".as_bytes(),
+                &frame[..],
+                "\r\n\r\n".as_bytes(),
+            ]
+            .concat(),
+        );
+
+        Poll::Ready(Some(Ok(body)))
     }
-}
-
-#[get("/video")]
-async fn video() -> impl Responder {
-    let frame_fn = get_frame_fn();
-    VideoFrame { frame: frame_fn() }
 }
