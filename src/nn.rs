7196341fb3ca2438ec8187ad784eaa2cee412229
Simon Gasse <sgasse@users.noreply.github.com>
2021-11-05 17:43:40 +0100

    Add disclaimer

--- src/nn.rs
+++ src/nn.rs
@@ -61,6 +61,7 @@ pub fn get_preproc_func(model_name: &str) -> Result<Box<dyn Fn(RgbImage) -> Tens
         let image: Tensor = tract_ndarray::Array4::from_shape_fn(
             (1, 3, height as usize, width as usize),
             |(_, c, y, x)| {
+                // Note: Mean/std are from MobileNet, not from Ultraface, but work well
                 let mean = [0.485, 0.456, 0.406][c];
                 let std = [0.229, 0.224, 0.225][c];
                 (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
