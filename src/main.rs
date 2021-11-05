a2ca90de7bd1a85fb685e153534b0df58bc5464c
Simon Gasse <sgasse@users.noreply.github.com>
2021-11-05 17:42:13 +0100

    Add argument parsing for port and IP

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,19 @@ use actix_web::App;
 use actix_web::HttpServer;
 use env_logger::TimestampPrecision;
 use libwebcam_onnx::routes::{face_detection, index, video_stream};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "InferCam")]
+struct Opts {
+    /// Port on which to serve
+    #[structopt(short, long, default_value = "8080")]
+    port: u32,
+
+    /// Bind to all IP addresses
+    #[structopt(short, long)]
+    bindall: bool,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -9,13 +22,19 @@ async fn main() -> std::io::Result<()> {
         .format_timestamp(Some(TimestampPrecision::Millis))
         .init();
 
+    let opts = Opts::from_args();
+    let bind_ip = match opts.bindall {
+        true => "0.0.0.0",
+        false => "127.0.0.1",
+    };
+
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(video_stream)
             .service(face_detection)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("{}:{}", bind_ip, opts.port))?
     .run()
     .await
 }
