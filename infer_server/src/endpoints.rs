12c15e17cf9faa329960c23215d19d253b7dcc4a
Simon Gasse <sgasse@users.noreply.github.com>
2022-10-08 16:42:53 +0200

    Relay to pubsub

--- infer_server/src/endpoints.rs
+++ infer_server/src/endpoints.rs
@@ -1,8 +1,62 @@
-use axum::extract::BodyStream;
+use crate::pubsub::NamedPubSub;
+use axum::{
+    extract::{BodyStream, Query},
+    Extension,
+};
 use futures::stream::StreamExt;
-use std::fs::File;
+use serde::Deserialize;
 use std::io::Cursor;
 use std::io::Write;
+use std::{fs::File, sync::Arc};
+
+#[derive(Debug, Deserialize)]
+pub struct RecvJpgsParams {
+    #[serde(default)]
+    name: Option<String>,
+}
+
+pub async fn recv_named_jpg_streams(
+    Extension(pubsub): Extension<Arc<NamedPubSub>>,
+    Query(params): Query<RecvJpgsParams>,
+    mut stream: BodyStream,
+) {
+    let name = params.name.unwrap_or("unknown".into());
+    log::info!("Receiving stream for name {}", &name);
+    let sender = pubsub.get_sender(&name).await;
+
+    let mut buf = Cursor::new(vec![0_u8; 100000]);
+    while let Some(Ok(data)) = stream.next().await {
+        log::debug!("Data length {}", data.len());
+        match data.ends_with("\r\n".as_bytes()) {
+            true => {
+                log::debug!("Skipping header {:?}", data);
+            }
+            false => {
+                // No header
+                match data.ends_with("\n\n".as_bytes()) {
+                    true => {
+                        // The last two bytes are the separation marker
+                        buf.write_all(&data[..(data.len() - 2)]).expect("write");
+                        log::debug!("Buf position {}", buf.position());
+                        log::debug!("Sending buffer");
+
+                        let send_data = buf.get_ref()[0..(buf.position() as usize)].to_vec();
+                        if let Err(_) = sender.send(send_data) {
+                            log::warn!("Error sending for channel {}", &name);
+                        }
+
+                        buf.set_position(0);
+                    }
+                    false => {
+                        log::debug!("Writing {} bytes", data.len());
+                        buf.write_all(&data).expect("write");
+                        log::debug!("Buf position {}", buf.position());
+                    }
+                }
+            }
+        }
+    }
+}
 
 pub async fn recv_jpgs(mut stream: BodyStream) {
     let mut counter = 0;
